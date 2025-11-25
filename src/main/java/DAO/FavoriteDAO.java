7e08f82411bf3d43d882bdf5feb8f55b8a40dc19
nghiado2803 <nghiadvpy00194@gmail.com>
2025-11-25 07:37:20 +0700

    Create FavoriteDAO.java

---
+++ src/main/java/DAO/FavoriteDAO.java
@@ -0,0 +1,8 @@
+package DAO;
+
+
+import Entity.Favorite;
+
+public interface FavoriteDAO extends GenericDAO<Favorite, Long> {
+    // Thêm các phương thức đặc thù của FavoriteDAO ở đây (ví dụ: findByUserIdAndVideoId)
+}
