b3b92153a88192de7689b1781a2151151f1b0bde
nghiado2803 <nghiadvpy00194@gmail.com>
2025-11-27 08:06:57 +0700

    Update FavoriteDAO.java

--- src/main/java/DAO/FavoriteDAO.java
+++ src/main/java/DAO/FavoriteDAO.java
@@ -1,8 +1,16 @@
 package DAO;
 
-
 import Entity.Favorite;
+import java.util.List;
 
-public interface FavoriteDAO extends GenericDAO<Favorite, Long> {
-    // Thêm các phương thức đặc thù của FavoriteDAO ở đây (ví dụ: findByUserIdAndVideoId)
+public interface FavoriteDAO extends AbstractDAO<Favorite> {
+
+    // Phương thức chuyên biệt cho Favorite
+    Favorite findByUserAndVideo(String userId, String videoId);
+
+    List<Favorite> findFavoritesByUserId(String userId, int page, int pageSize);
+
+    // Dùng cho báo cáo
+    Long countFavoritesByVideoId(String videoId);
+    public List<Favorite> findByUserId(String userId);
 }
