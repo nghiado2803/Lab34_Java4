8dd0f7077c90d3929fe537350e90b3da85b2301b
nghiado2803 <nghiadvpy00194@gmail.com>
2025-11-27 08:07:49 +0700

    Update VideoDAO.java

--- src/main/java/DAO/VideoDAO.java
+++ src/main/java/DAO/VideoDAO.java
@@ -3,6 +3,13 @@ package DAO;
 import Entity.Video;
 import java.util.List;
 
-public interface VideoDAO extends GenericDAO<Video, String> {
-    List<Video> searchByTitle(String keyword);
+public interface VideoDAO extends AbstractDAO<Video> {
+
+    // Phương thức chuyên biệt cho Video
+    List<Video> findActiveVideos(int page, int pageSize);
+
+    List<Video> findByKeyword(String keyword, int page, int pageSize);
+
+    List<Video> findRandomVideos(int limit);
+    public List<Video> searchByTitle(String keyword);
 }
