844a0b19a5d11275eb8532d99ac2553aaa720c48
nghiado2803 <nghiadvpy00194@gmail.com>
2025-12-06 15:36:22 +0700

    Create LogsDAOImpl.java

---
+++ src/main/java/DAOImpl/LogsDAOImpl.java
@@ -0,0 +1,8 @@
+package DAOImpl;
+
+import DAO.LogsDAO;
+import Entity.Logs;
+
+public class LogsDAOImpl extends AbstractDAOImpl<Logs> implements LogsDAO {
+    // Không cần override gì cả, dùng hết của cha
+}
