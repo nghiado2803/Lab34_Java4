cf8df1430f6964ed2e729dcd4c55244ef0b19f03
nghiado2803 <nghiadvpy00194@gmail.com>
2025-12-06 15:36:59 +0700

    Create AdminServlet.java

---
+++ src/main/java/Servlet/AdminServlet.java
@@ -0,0 +1,23 @@
+package Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({
+        "/admin/video",
+        "/admin/user",
+        "/admin/like",
+        "/admin/share"
+})
+public class AdminServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/views/page.jsp").forward(req, resp);
+    }
+}
