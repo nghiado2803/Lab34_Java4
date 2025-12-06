0d91278e340f5551171d6ae7266c134f4b0c4b30
nghiado2803 <nghiadvpy00194@gmail.com>
2025-12-06 15:37:06 +0700

    Create VideoServlet.java

---
+++ src/main/java/Servlet/VideoServlet.java
@@ -0,0 +1,23 @@
+package Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({
+        "/video/list",
+        "/video/detail/*",
+        "/video/like/*",
+        "/video/share/*"
+})
+public class VideoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/views/page.jsp").forward(req, resp);
+    }
+}
